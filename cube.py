ea7277512776510b852846b25749584367338d5c
robertecurtin <robertecurtin@gmail.com>
2014-10-16 10:07:39 -0400

    New SQL command to simplify parameterization
    Now all functions of MTG_Cube call a self.sql function. This function
    properly formats all parameters and inserts them into the SQL query, and
    executes the command.
    
    Next up: Make sure all functions necessary to statistics exist. Also,
    try to generalize functions to take in any number of restrictions
    (filter columns).

--- cube.py
+++ cube.py
@@ -17,41 +17,36 @@ class MTG_Cube:
         
         self.db.commit()
 
-    def fetchall(self):
+    def fetchall(self): # Returns a list of items from SQLite
         return [x[0] for x in self.cursor.fetchall()]
 
-    def fetchone(self):
+    def fetchone(self): # Fetches a single value from SQLite, such as the number returned by COUNT()
         return self.cursor.fetchone()[0]
 
-    def get_all_distinct(self, table_name):
-        table_name = '"' + table_name + '"' # Necessary to insert into SQLite statement
-        self.cursor.execute("SELECT DISTINCT {} FROM Cards".format(table_name))
+    def sql(self, command, *parameters): # Runs the given command through SQLite, formatted with the given parameters
+        parameters = tuple(['"' + str(x) + '"' for x in parameters]) # Necessary to insert into SQLite statement
+        self.cursor.execute(command.format(*parameters))
+
+    def get_all_distinct(self, column_name):
+        self.sql("SELECT DISTINCT {} FROM Cards", column_name)
         return self.fetchall()
 
-    def get_total_number(self, table_name):
-        table_name = '"' + table_name + '"'
-        self.cursor.execute("SELECT COUNT({}) AS Number FROM Cards WHERE {}=?", (table_name, table_name))
+    def get_total_number(self, column_name):
+        self.sql("SELECT COUNT({}) AS Number FROM Cards",column_name)
         return self.fetchone()
 
-    def get_number_within(self, table_to_count, filter_table, filter_value):
-        table_to_count = '"' + table_to_count + '"'
-        filter_table = '"' + filter_table + '"'
-        self.cursor.execute("SELECT COUNT({}) AS Number FROM Cards WHERE {}=?",
-                            (table_to_count, filter_table, filter_value))
-        return self.fetchall()
+    def get_number_within(self, column_to_count, filter_column, filter_value):
+        self.sql("SELECT COUNT({0}) AS Number FROM Cards WHERE {1}={2}",
+                 column_to_count, filter_column, filter_value)
+        return self.fetchone()
         
-    def get_distinct_within(self, table_to_search, filter_table, filter_value):
-        table_to_search = '"' + table_to_search + '"'
-        filter_table = '"' + filter_table + '"'
-        self.cursor.execute("SELECT DISTINCT {} FROM Cards WHERE {}=?",
-                            (table_to_search, filter_table, filter_value))
+    def get_distinct_within(self, column_to_search, filter_column, filter_value):
+        self.sql("SELECT DISTINCT {} FROM Cards WHERE {}={}",
+                 column_to_search, filter_column, filter_value)
         return self.fetchall()
 
-    def get_number_within_two(self, table_to_count, filter_table_1, filter_value_1, filter_table_2, filter_value_2):
-        table_to_count = '"' + table_to_count + '"'
-        filter_table_1 = '"' + filter_table_1 + '"'
-        filter_table_2 = '"' + filter_table_2 + '"'
-        self.cursor.execute("SELECT COUNT({}) AS Number FROM Cards WHERE {}=? AND {}=?",
-                            (table_to_count, filter_table_1, filter_value_1, filter_table_2, filter_value_2))
-        return self.fetchall()
+    def get_number_within_two(self, column_to_count, filter_column_1, filter_value_1, filter_column_2, filter_value_2):
+        self.sql("SELECT COUNT({}) AS Number FROM Cards WHERE {}={} AND {}={}",
+                 column_to_count, filter_column_1, filter_value_1, filter_column_2, filter_value_2)
+        return self.fetchone()
 
--- cube_stats.py
+++ cube_stats.py
@@ -18,50 +18,23 @@ def main():
             print '    CMC: {}'.format(cmc)
 
             color_total = cube.get_total_number(color)
-            cmc_in_color = cube.get_number_within(cmc, color)
+            cmc_in_color = cube.get_number_within(cmc, 'Color', color)
             percent = 100 * cmc_in_color / color_total
             print '        My cube: {}/{}, {}%'.format(
                 cmc_in_color, color_total, percent)
             
             other_color_total = other_cube.get_total_number(color)
-            other_cmc_in_color = other_cube.get_number_within(cmc, color)
+            other_cmc_in_color = other_cube.get_number_within(cmc, 'Color', color)
             other_percent = 100 * other_cmc_in_color / other_color_total
             print '        Other: {}/{}, {}%'.format(
                 other_cmc_in_color, other_color_total, other_percent)
             
-
-def main2():
-    db = sqlite3.connect(':memory:')
-    cursor = db.cursor()
-
-    print "Creating table"
-    cursor.execute('CREATE TABLE cards_cube (Card_Name text, Color text, Card_Type text, CMC integer, BLANK text)')
-    cursor.execute('CREATE TABLE cards_360 (Card_Name text, Color text, Card_Type text, CMC integer, BLANK text)')
-    cursor.execute('CREATE TABLE cards_720 (Card_Name text, Color text, Card_Type text, CMC integer, BLANK text)')
-    db.commit()
-
-    print 'Opening file'
-
-    cube_file = open('cube.csv','rb')
-    cube_reader = csv.reader(cube_file, delimiter=',',quotechar='"')
-
-    print 'Reading file'
-    for line in cube_reader:
-        cursor.execute('INSERT INTO cards_cube VALUES (?,?,?,?,?)',line)
-    cube_file.close()
+def test():
+    cube = MTG_Cube('cube')
+    print cube.get_number_within('CMC', 'Color', 'MONO_RED')
+    for x in cube.get_all_distinct('Color'):
+        print x
+    print cube.get_total_number('Color')
+    print cube.get_distinct_within('CMC', 'Color', 'MONO_RED')
     
-    db.commit()
-
-    print 'Selecting black cards'
-    #cursor.execute("SELECT COUNT(Color) AS NumberOfBlack FROM Cards WHERE Color='MONO_BLACK'")
-    cursor.execute("SELECT DISTINCT Color FROM cards_cube")
-    datas = cursor.fetchall()
-    color_dictionary = {}
-    for data in datas:
-        cursor.execute("SELECT COUNT(Color) AS Number FROM cards_cube WHERE Color=?", data)
-        number = cursor.fetchone()
-        color_dictionary[data] = number[0]
-        
-    db.close()
-
 main()
